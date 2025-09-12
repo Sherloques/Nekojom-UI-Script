d2debd5f192d455a0b1a3e07866475feb4581132
Nekojom <89567952+Sherloques@users.noreply.github.com>
2025-09-12 12:49:54 +0700

    Update NekoJomUI.lua

--- NekoJomUI.lua
+++ NekoJomUI.lua
@@ -17,6 +17,7 @@ local Window = Fluent:CreateWindow({
 local Tabs = {
     Home = Window:AddTab({ Title = "Home", Icon = "home" }),
     Script = Window:AddTab({ Title = "Script", Icon = "rbxassetid://10734907168" }),
+    Fish = Window:AddTab({ Title = "Fish It", Icon = "rbxassetid://10709775894" }),
     Players = Window:AddTab({ Title = "Players", Icon = "rbxassetid://114799287720031" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
@@ -37,7 +38,11 @@ local Other = {
     "Anti AFK",
     "infiniteyield",
 }
-
+local FishIt = {
+    "Bonk Hub",
+    "CHIYO",
+    "BEBAS",
+}
 
 do
     pcall(function()
@@ -60,6 +65,33 @@ do
         end
     })
     end)
+
+    Tabs.Script:AddSection("[ Fish It / มีคีย์ ]")
+
+    local Dropdown = Tabs.Script:AddDropdown("Dropdown", {
+        Title = "Select Scripts",
+        Values = FishIt,
+        Multi = false,
+        Default = "เลือกสคริปต์",
+    })
+
+    Dropdown:OnChanged(function(Value)
+        FishIts = Value
+    end)
+
+    Tabs.Script:AddButton({
+        Title = "Click To Execute",
+        Description = "กดเพื่อรันสคริปต์",
+        Callback = function()
+            if FishIts == "Bonk Hub" then
+                loadstring(game:HttpGet("https://bonkhub.online/loader.lua",true))();
+            elseif FishIts == "CHIYO" then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/kaisenlmao/loader/refs/heads/main/chiyo.lua"))();
+            elseif FishIts == "BEBAS" then
+                loadstring(game:HttpGet("https://gist.githubusercontent.com/OmarBinLadek/25e75529e18b38e5d38beab9126fc004/raw/6d1c1c5e0e91373654d2f85ebb65e8221728e26d/freefishit.lua"))();
+            end
+        end
+    })
 
     Tabs.Script:AddSection("[ key / มีคีย์ ]")
 
